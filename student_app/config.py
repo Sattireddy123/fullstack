d6a62dd61cea14478c4c38a8a87e4668ec8188de
Sattireddy123 <dsattireddy7981@gmail.com>
2026-04-16 15:27:30 +0530

    Add files via upload

---
+++ student_app/config.py
@@ -0,0 +1,4 @@
+class Config:
+    SECRETE_KEY='dev_secrete_key'
+    SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:Sattireddy%40123!@localhost/student_db'
+    SQLALCHEMY_TRACK_MODIFICATION = False
